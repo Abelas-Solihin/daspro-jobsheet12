85b3d8c00bc7d43a782b577654cc5da6e9c0d0ec
Abelas-Solihin <abelassolihin@gmail.com>
2024-11-28 15:14:32 +0700

    menambah deretan pangkat

--- percobaan2.java
+++ percobaan2.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 public class percobaan2 {
     public static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
-        } else  {
-            return (x * hitungPangkat(x, y - 1));
+            System.out.print(" = "); 
+            return 1;
+        } else {
+            System.out.print(x); 
+            if (y > 1) { 
+                System.out.print(" x "); 
+            }
+            return x * hitungPangkat(x, y - 1); 
         }
     }
     public static void main(String[] args) {
